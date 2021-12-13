87150687073e8cf5d0d2ae1693dd9b6cc91ea3d2
Brenda Holloway <brendahol@gmail.com>
2021-12-13 12:24:22 -0500

    added curses

--- 2021/puzzle13.py
+++ 2021/puzzle13.py
@@ -1,31 +1,39 @@
 import re
+import curses
 
 def read_input():
+    first_points = None
     with open('puzzle13.dat') as f:
         points = set()
         for line in f:
             line = line.strip()
-            if line.startswith('fold'):
-                points = fold(line, points)
+            m = re.match(r'fold along ([xy])=(\d+)', line)
+            if m:
+                points = fold(m.group(1), int(m.group(2)), points)
+                first_points = first_points or len(points)
             elif line:
                 points.add(tuple(map(int, line.split(','))))
-    return points
+    return first_points, points
 
-def fold(line, points):
-    m = re.match(r'fold along ([xy])=(\d+)', line)
-    value = int(m.group(2))
-    if m.group(1) == 'x':
+def fold(axis, value, points):
+    if axis == 'x':
         fn = lambda p: p if p[0] < value else (value - (p[0] - value), p[1])
     else:
         fn = lambda p: p if p[1] < value else (p[0], value - (p[1] - value))
     return set(fn(point) for point in points)
 
-points = read_input()
-max_x = max(points, key=lambda x: x[0])[0]
-max_y = max(points, key=lambda x: x[1])[1]
+def draw(first_points, points):
+    win = curses.initscr()
+    win.clear()
 
-grid = [['#' if (x, y) in points else ' ' for x in range(max_x + 1)]
-        for y in range(max_y + 1)]
+    win.addstr(0, 0, 'Part 1: {}'.format(first_points))
 
-for row in grid:
-    print(''.join(row))
+    for p in points:
+        win.addch(p[1]+2, p[0], '#')
+    
+    win.refresh()
+    win.getch()
+    curses.endwin()
+
+fp, p = read_input()
+draw(fp, p)
