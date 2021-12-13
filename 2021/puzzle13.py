d8802936637a500a5c6e99d18348167fc06c7029
Brenda Holloway <brendahol@gmail.com>
2021-12-13 08:18:26 -0500

    Add lambdas into fold to reduce redundancy

--- 2021/puzzle13.py
+++ 2021/puzzle13.py
@@ -12,20 +12,20 @@ def read_input():
     return points
 
 def fold(line, points):
-    m = re.match(r'fold along ([xy])\=(\d+)', line)
+    m = re.match(r'fold along ([xy])=(\d+)', line)
     value = int(m.group(2))
     if m.group(1) == 'x':
-        new_points = set(point if point[0] <= value else (
-            value - (point[0] - value), point[1]) for point in points)
+        fn = lambda p: p if p[0] < value else (value - (p[0] - value), p[1])
     else:
-        new_points = set(point if point[1] <= value else (
-            point[0], value - (point[1] - value)) for point in points)
-    return new_points
+        fn = lambda p: p if p[1] < value else (p[0], value - (p[1] - value))
+    return set(fn(point) for point in points)
 
 points = read_input()
 max_x = max(points, key=lambda x: x[0])[0]
 max_y = max(points, key=lambda x: x[1])[1]
+
 grid = [['#' if (x, y) in points else ' ' for x in range(max_x + 1)]
         for y in range(max_y + 1)]
+
 for row in grid:
     print(''.join(row))
