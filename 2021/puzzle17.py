b56e006f97ef363e79fab4083ecfe1a1783cdee5
Brenda Holloway <brendahol@gmail.com>
2021-12-17 08:28:48 -0500

    Reformatted for readability

--- 2021/puzzle17.py
+++ 2021/puzzle17.py
@@ -11,22 +11,27 @@ def fire(v1, v2, dvf, fvt):
             if x >= v1 and x <= v2:
                 yield dv, max_x
                 break
-            elif fvt(x, v1, v2): break
+            elif fvt(x, v1, v2):
+                break
             x += dv1
             dv1 = dvf(dv1)
 
 def trace(dx, dy):
     x, y = 0, 0
     for _ in range(1000):
-        if x > x2 or y < y1: return False
-        if x >= x1 and y <= y2: return True
+        if x > x2 or y < y1:
+            return False
+        if x >= x1 and y <= y2:
+            return True
         x, y = x + dx, y + dy
         dx, dy = dxf(dx), dyf(dy)
 
 m = re.search(r"x=([\d-]+)\.\.([\d-]+), y=([\d-]+)\.\.([\d-]+)", data)
-x1, x2, y1, y2 = int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4))
-dxf = lambda dx: 0 if dx == 0 else dx - abs(dx)/dx
-dyf = lambda dy: dy-1
+x1, x2, y1, y2 = int(m.group(1)), int(
+    m.group(2)), int(m.group(3)), int(m.group(4))
+
+def dxf(dx): return 0 if dx == 0 else dx - abs(dx)/dx
+def dyf(dy): return dy-1
 
 possible_xs = [xy for xy in fire(x1, x2, dxf, lambda x, _, v2: x > v2)]
 possible_ys = [yy for yy in fire(y1, y2, dyf, lambda y, v1, _: y < v1)]
