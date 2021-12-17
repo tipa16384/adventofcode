014a26c075d11771cd1b696ae0d9fb082d446b80
Brenda Holloway <brendahol@gmail.com>
2021-12-17 11:04:51 -0500

    Based on discussion on Reddit, adjusting solution.

--- 2021/puzzle17.py
+++ 2021/puzzle17.py
@@ -1,16 +1,15 @@
 import re
 
-data = "target area: x=56..76, y=-162..-134"
+data = "target area: x=352..377, y=-49..-30"
 
 def fire(v1, v2, dvf, fvt):
     max_x = -10000
-    for dv in range(-100, 300):
+    for dv in range(-500, 5000):
         dv1, x = dv, 0
         for t in range(1000):
             max_x = max(max_x, x)
             if x >= v1 and x <= v2:
-                yield dv, max_x
-                break
+                yield dv, max_x, t
             elif fvt(x, v1, v2):
                 break
             x += dv1
@@ -30,13 +29,14 @@ m = re.search(r"x=([\d-]+)\.\.([\d-]+), y=([\d-]+)\.\.([\d-]+)", data)
 x1, x2, y1, y2 = int(m.group(1)), int(
     m.group(2)), int(m.group(3)), int(m.group(4))
 
-def dxf(dx): return 0 if dx == 0 else dx - abs(dx)/dx
+def dxf(dx): return 0 if dx == 0 else dx - abs(dx)//dx
 def dyf(dy): return dy-1
 
 possible_xs = [xy for xy in fire(x1, x2, dxf, lambda x, _, v2: x > v2)]
 possible_ys = [yy for yy in fire(y1, y2, dyf, lambda y, v1, _: y < v1)]
+xy_times = set(t for _, _, t in possible_xs).intersection(set(t for _, _, t in possible_ys))
 
-print('Part 1:', max(possible_ys)[1])
+print('Part 1:', max(y for _, y, t in possible_ys if t in xy_times))
 
 print('Part 2:', sum(1 for dx in range(min(possible_xs)[0], max(possible_xs)[
     0]+1) for dy in range(min(possible_ys)[0], max(possible_ys)[0]+1) if trace(dx, dy)))
