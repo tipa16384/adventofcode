225b4ea804543f04118014083ebdbdd5ec00d00a
Brenda Holloway <brendahol@gmail.com>
2021-12-26 19:55:50 -0500

    2015 day 7 solution

---
+++ 2015/circuit7.py
@@ -0,0 +1,51 @@
+ops = {
+    'AND': lambda x, y: x & y,
+    'OR': lambda x, y: x | y,
+    'LSHIFT': lambda x, y: x << y,
+    'RSHIFT': lambda x, y: x >> y,
+    'NOT': lambda x: ~x
+}
+
+def read_data() -> dict:
+    circuit = dict()
+
+    with open("circuit7.dat") as f:
+        for line in f.read().split('\n'):
+            line = line.split(' -> ')
+            circuit[line[1]] = line[0]
+
+    return circuit
+
+def expand_circuit(value: str) -> int:
+    global circuit
+
+    if isinstance(value, int):
+        return value
+
+    if value.isnumeric():
+        val = int(value)
+    else:
+        values = value.split(' ')
+
+        if len(values) == 1:
+            val = expand_circuit(circuit[values[0]])
+        elif values[0] == 'NOT':
+            val = ops['NOT'](expand_circuit(values[1]))
+        else:
+            val = ops[values[1]](expand_circuit(values[0]),
+                                 expand_circuit(values[2]))
+
+    circuit[value] = val
+
+    return val
+
+
+circuit = read_data()
+a_value = expand_circuit('a')
+
+print(f"Part 1: {a_value}")
+
+circuit = read_data()
+circuit['b'] = a_value
+
+print(f"Part 2: {expand_circuit('a')}")
