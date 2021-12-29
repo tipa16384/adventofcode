f73105d2cd0a89daccece14ba9b94724dd35a0f8
Brenda Holloway <brendahol@gmail.com>
2021-12-29 10:25:02 -0500

    2015 Day 12 part 2

--- 2015/accounting12.py
+++ 2015/accounting12.py
@@ -1,5 +1,24 @@
-import re
+from re import findall
+from json import loads
+from multipledispatch import dispatch
+
+@dispatch(int)
+def sum_objs(data: int) -> int: return data
+
+@dispatch(str)
+def sum_objs(_: str) -> int: return 0
+
+@dispatch(list)
+def sum_objs(data: list) -> int: return sum(sum_objs(x) for x in data)
+
+@dispatch(dict)
+def sum_objs(data: dict) -> int:
+    return 0 if 'red' in data.values() else sum(sum_objs(x) for x in data.values())
 
 with open('accounting12.dat') as f: data = f.read()
 
-print ("Part 1:", sum(int(x) for x in re.findall(r'\-{,1}\d+', data)))
+print ("Part 1:", sum(int(x) for x in findall(r'\-{,1}\d+', data)))
+
+data = loads(data)
+
+print ("Part 2:", sum_objs(data))
