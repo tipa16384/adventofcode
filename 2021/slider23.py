c05e1732736a53f8b82ff14ae14d4de8891dd5c1
Brenda Holloway <brendahol@gmail.com>
2022-01-05 23:20:57 -0500

    Making progress towards 2021.23 solution.

---
+++ 2021/slider23.py
@@ -0,0 +1,97 @@
+def read_data() -> list:
+    with open('puzzle23.dat') as f:
+        return f.read().split('\n')
+
+move_values = {'A': 1, 'B':10, 'C':100, 'D':1000 }
+home_x = {'A': 3, 'B': 5, 'C': 7, 'D': 9}
+
+def calc_dist(data: list) -> int:
+    total_dist = 0
+
+    for x, c in enumerate(data[1]):
+        if c in move_values.keys():
+            total_dist += (abs(x - home_x[c]) + 1) * move_values[c]
+    
+    for y, row in enumerate(data[2:]):
+        for x, c in enumerate(row):
+            if c in move_values.keys() and x != home_x[c]:
+                total_dist += (2 + y + abs(x - home_x[c])) * move_values[c]
+    
+    return total_dist
+
+def can_go_home(data: list, c: str) -> bool:
+    for row in data[2:]:
+        occupant = row[home_x[c]]
+        if occupant in move_values.keys() and occupant != c:
+            #print (f"{c} can't go home, {occupant} is in the house")
+            return False
+    #print (f"{c} can go home, no trespassers")
+    return True
+
+def hallway_not_blocked(data: list, c: str, x: int) -> bool:
+    step = -1 if x > home_x[c] else 1
+    while x != home_x[c]:
+        x += step
+        if data[1][x] != '.':
+            #print (f"{c} can't go home, hallway blocked at {x}")
+            return False
+    #print (f"{c} can go home, hallway not blocked")
+    return True
+
+def yield_moves(data: list) -> None:
+    for x, c in enumerate(data[1]):
+        if c in move_values.keys() and can_go_home(data, c) and hallway_not_blocked(data, c, x):
+            dist = abs(x - home_x[c])
+            dest_y = 1
+            for y, row in enumerate(data[2:]):
+                if row[home_x[c]] == '.':
+                    dist += 1
+                    dest_y = y + 2
+
+            print (f"move {c} from {x},1 to {home_x[c]},{dest_y}")
+            yield (x, 1, home_x[c], dest_y, dist * move_values[c])
+    
+    for y in range(2, len(data)):
+        for x in home_x.values():
+            c = data[y][x]
+            if c in move_values.keys() and (x != home_x[c] or not can_go_home(data, c)):
+                if any(row[x] != '.' for row in data[1:y]):
+                    continue
+                dx = x - 1
+                while data[1][dx] == '.':
+                    if dx not in home_x.values():
+                        dist = y - 1 + x - dx
+                        print (f"move {c} from {x},{y} into the hallway at {dx},1")
+                        yield (x, y, dx, 1, dist * move_values[c])
+                    dx -= 1
+                dx = x + 1
+                while data[1][dx] == '.':
+                    if dx not in home_x.values():
+                        dist = y - 1 + dx - x
+                        print (f"move {c} from {x},{y} into the hallway at {dx},1")
+                        yield (x, y, dx, 1, dist * move_values[c])
+                    dx += 1
+
+def make_move(data: list, move: tuple) -> list:
+    new_data = list(data)
+    s = list(c for c in data[move[1]])
+    s[move[0]] = '.'
+    new_data[move[1]] = ''.join(s)
+    s = list(c for c in data[move[3]])
+    s[move[2]] = data[move[1]][move[0]]
+    new_data[move[3]] = ''.join(s)
+    return new_data
+
+def print_board(data: list) -> None:
+    for row in data:
+        print (''.join(row))
+    print ()
+
+data = read_data()
+print(calc_dist(data))
+
+for m in yield_moves(data):
+    print(m)
+    print_board(make_move(data, m))
+
+
