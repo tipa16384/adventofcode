f65ef30735f0fc040de1d6239b98e484fad70e3e
Brenda Holloway <brendahol@gmail.com>
2024-12-05 17:55:48 -0500

    2024.5 revised

--- 2024/aoc2024.py
+++ 2024/aoc2024.py
@@ -56,13 +56,11 @@ def day_4_rest():
 
 @app.route('/2024/5', methods=['POST'])
 def day_5_rest():
-    from day5funcs import day5_data, alles_in_Ordnung, mach_es_richtig
+    from day5funcs import day5_data, beide_teile
     file, success = get_file_from_request()
     if not success:
         return file, 400
-    page_ordering_map, updates = day5_data(file)
-    return build_response(*run_threads((alles_in_Ordnung, page_ordering_map, updates), 
-                                       (mach_es_richtig, page_ordering_map, updates)))
+    return build_response(*beide_teile(*day5_data(file)))
 
 @app.route('/2016/2', methods=['POST'])
 def day20162_rest():
--- 2024/day5funcs.py
+++ 2024/day5funcs.py
@@ -1,46 +1,24 @@
-from collections import defaultdict
 from functools import cmp_to_key
 
 def day5_data(file) -> tuple:
     data = file.read().decode('utf-8')
     ordering_lines, page_lines = data.split('\n\n')
-    
-    page_ordering_map = defaultdict(set)
-    for line in ordering_lines.splitlines():
-        prior_page, page = map(int, line.split('|'))
-        page_ordering_map[page].add(prior_page)
-    
-    updates = [list(map(int, line.split(','))) for line in page_lines.splitlines()]
-    return page_ordering_map, updates
+    return [tuple(map(int, line.split('|'))) for line in ordering_lines.splitlines()], \
+            [list(map(int, line.split(','))) for line in page_lines.splitlines()]
 
-def druckbar(page_ordering_map: dict, pages: list) -> bool:
-    seen_pages = set()
-    for page in reversed(pages):
-        if page_ordering_map[page].intersection(seen_pages):
-            return False
-        seen_pages.add(page)
-    return True
+def beide_teile(page_order_list: list, updates: list) -> tuple:
+    part2_sum = part1_sum = 0
 
-def page_ordering(page_ordering_map: dict, page_tuple_1: tuple, page_tuple_2: tuple) -> int:
-    if page_tuple_2[1] in page_ordering_map[page_tuple_1[1]]:
-        return 1
-    elif page_tuple_1[1] in page_ordering_map[page_tuple_2[1]]:
-        return -1
-    else:
-        return page_tuple_1[0] - page_tuple_2[0]
+    for update in updates:
+        sorted_update = sorted(update, key=cmp_to_key(lambda x, y: page_ordering(page_order_list, x, y)))
+        middle_element = sorted_update[len(update) // 2]
+        if sorted_update == update:
+            part1_sum += middle_element
+        else:
+            part2_sum += middle_element
+    return part1_sum, part2_sum
 
-def test_druckbar(page_ordering_map: dict, pages: list) -> list:
-    compare = lambda x, y: page_ordering(page_ordering_map, x, y)
-    indexed_pages = list(enumerate(pages))
-    sorted_pages = sorted(indexed_pages, key=cmp_to_key(compare))
-    return [page for _, page in sorted_pages]
-
-def alles_in_Ordnung(page_ordering_map: dict, updates: list) -> int:
-    return sum(update[len(update) // 2] for update in updates if druckbar(page_ordering_map, update))
-
-def mach_es_richtig(page_ordering_map: dict, updates: list) -> int:
-    return sum(
-        test_druckbar(page_ordering_map, update)[len(update) // 2]
-        for update in updates
-        if not druckbar(page_ordering_map, update)
-    )
+def page_ordering(page_order_list: list, page1: int, page2: int) -> int:
+    if (page1,page2) in page_order_list: return -1
+    if (page2,page1) in page_order_list: return 1
+    return 0
